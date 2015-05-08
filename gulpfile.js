9b84096422ab6dee6d2cb2a4a352cb283c8de818
briankalwat <brian@briankalwat.com>
2015-05-08 18:15:54 -0400

    updated gulpfile to compile multiple scss files

--- gulpfile.js
+++ gulpfile.js
@@ -76,7 +76,7 @@ var scssProcessing = lazypipe()
 
 //styles task
 gulp.task('styles', function() {
-	return gulp.src('src/scss/master.scss').pipe(scssProcessing());
+	return gulp.src(scssGlob).pipe(scssProcessing());
 });
 
 //scripts individual task
